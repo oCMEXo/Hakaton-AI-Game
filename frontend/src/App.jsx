a28b8a64b99cfcf47df379a4f2903914e00036de
artem <artsiomharminovich05@gmail.com>
2025-05-08 23:24:32 +0300

    Create PageQuestionReceipt.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import MainPage from "./Components/Page/MainPage.jsx";
+import PageQuestionReceipt from "./Components/Page/PageQuestionReceipt.jsx";
 
 
 function App() {
     return (
-    <MainPage/>
+    <PageQuestionReceipt/>
   )
 }
 
---
+++ frontend/src/Components/Page/PageQuestionReceipt.jsx
@@ -0,0 +1,12 @@
+
+import QueryTopicExplorer from "../QueryTopicExplorer.jsx";
+import Header from "../Lauout/Header.jsx";
+
+export default function PageQuestionReceipt() {
+    return (
+        <>
+            <Header />
+            <QueryTopicExplorer />
+        </>
+    );
+}
